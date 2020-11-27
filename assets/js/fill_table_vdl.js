281ca9e5c6f51e40727cc843f3d9615b4ca76f66
garethwalkom <walkga04@googlemail.com>
2020-11-27 15:54:18 +0100

    Implementation of live Virtual Distance League leaderboards

---
+++ assets/js/fill_table_vdl.js
@@ -0,0 +1,69 @@
+$.get(
+    "https://docs.google.com/spreadsheets/d/1qBFIcE44w9cEtGmw8ni2GM9ikraKUxHz-6uhQTUpe1c/export?format=csv&id=1qBFIcE44w9cEtGmw8ni2GM9ikraKUxHz-6uhQTUpe1c&gid=615076828", 
+    function ( data ) {
+        var text_entries = data.split('\r\n');
+        var tbl = document.querySelector('#overall-leaderboard');
+        var entry;
+        for (let i = 2; i <= 26; i++) {
+            entry = text_entries[i].split(',').slice(0,9);
+            var text = '\n<tr><td>' + entry[0] + '</td><td>' + entry[1] + '</td><td>' + entry[2] + '</td><td>' + entry[3] + '</td><td>' + entry[4] + '</td><td>' + entry[5] + '</td><td>' + entry[6] + '</td><td>' + entry[7] + '</td><td>' + entry[8] + '</td></tr>';
+            tbl.innerHTML += text;
+        }
+    }
+)
+
+$.get(
+    "https://docs.google.com/spreadsheets/d/1qBFIcE44w9cEtGmw8ni2GM9ikraKUxHz-6uhQTUpe1c/export?format=csv&id=1qBFIcE44w9cEtGmw8ni2GM9ikraKUxHz-6uhQTUpe1c&gid=570045132", 
+    function ( data ) {
+        var text_entries = data.split('\r\n');
+        var tbl = document.querySelector('#five');
+        var entry;
+        for (let i = 1; i <= 6; i++) {
+            entry = text_entries[i].split(',').slice(0,5);
+            var text = '\n<tr><td>' + entry[0] + '</td><td>' + entry[1] + '</td><td>' + entry[2] + '</td><td>' + entry[3] + '</td><td>' + entry[4] + '</td></tr>';
+            tbl.innerHTML += text;
+        }
+    }
+)
+
+$.get(
+    "https://docs.google.com/spreadsheets/d/1qBFIcE44w9cEtGmw8ni2GM9ikraKUxHz-6uhQTUpe1c/export?format=csv&id=1qBFIcE44w9cEtGmw8ni2GM9ikraKUxHz-6uhQTUpe1c&gid=1797755741", 
+    function ( data ) {
+        var text_entries = data.split('\r\n');
+        var tbl = document.querySelector('#first-ten');
+        var entry;
+        for (let i = 1; i <= 6; i++) {
+            entry = text_entries[i].split(',').slice(0,5);
+            var text = '\n<tr><td>' + entry[0] + '</td><td>' + entry[1] + '</td><td>' + entry[2] + '</td><td>' + entry[3] + '</td><td>' + entry[4] + '</td></tr>';
+            tbl.innerHTML += text;
+        }
+    }
+)
+
+$.get(
+    "https://docs.google.com/spreadsheets/d/1qBFIcE44w9cEtGmw8ni2GM9ikraKUxHz-6uhQTUpe1c/export?format=csv&id=1qBFIcE44w9cEtGmw8ni2GM9ikraKUxHz-6uhQTUpe1c&gid=83438411", 
+    function ( data ) {
+        var text_entries = data.split('\r\n');
+        var tbl = document.querySelector('#second-ten');
+        var entry;
+        for (let i = 1; i <= 6; i++) {
+            entry = text_entries[i].split(',').slice(0,5);
+            var text = '\n<tr><td>' + entry[0] + '</td><td>' + entry[1] + '</td><td>' + entry[2] + '</td><td>' + entry[3] + '</td><td>' + entry[4] + '</td></tr>';
+            tbl.innerHTML += text;
+        }
+    }
+)
+
+$.get(
+    "https://docs.google.com/spreadsheets/d/1qBFIcE44w9cEtGmw8ni2GM9ikraKUxHz-6uhQTUpe1c/export?format=csv&id=1qBFIcE44w9cEtGmw8ni2GM9ikraKUxHz-6uhQTUpe1c&gid=520056310", 
+    function ( data ) {
+        var text_entries = data.split('\r\n');
+        var tbl = document.querySelector('#half-marathon');
+        var entry;
+        for (let i = 1; i <= 6; i++) {
+            entry = text_entries[i].split(',').slice(0,5);
+            var text = '\n<tr><td>' + entry[0] + '</td><td>' + entry[1] + '</td><td>' + entry[2] + '</td><td>' + entry[3] + '</td><td>' + entry[4] + '</td></tr>';
+            tbl.innerHTML += text;
+        }
+    }
+)
--- vdl.md
+++ vdl.md
@@ -6,13 +6,74 @@ description: Sign up to the UUU Virtual Distance League
 nav-menu: false
 menu_bottom: false
 show_tile: true
+add_script: assets/js/fill_table_vdl.js
 ---
 
 Want to take part in some friendly competition, improve your riding and smash some personal bests all at the same time?
-We will be running a series of virtual distance/racing events over the next few weeks to test your speed and endurance. The events will run one per month until the end of the year. All you need to do is sign up, pick your distance, complete the ride on the relevant date, record it via GPS and submit a screenshot to us for verification.
-Your time will be entered into a leaderboard on our website and you will accumulate points for every race you take part in. 
-Full details, sign up form and rules are below.
+We have been running a series of virtual distance/racing events over several weeks to test your speed and endurance. The events are running one per month until the end of the year. All you need to do is sign up, pick your distance, complete the ride on the relevant date, record it via GPS and submit a screenshot to us for verification.
+Your time will be entered into our live leaderboard on our website and you will accumulate points for every race you take part in. 
 
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfP2xryt4G684LNwI-3ROs-mplrZ7aZ4jMtd3_P7RN4eSxVTg/viewform?embedded=true&entry.455468961=Okay" width="640" height="1473" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
-
-{% comment %} UUU Virtual Distance League sign up form embed {% endcomment %}
+<html>
+<h2>Overall Leaderboard</h2>
+	<table id='overall-leaderboard' style="width:100%">
+      <tr>
+        <th>Name</th>
+        <th colspan="2">Points 1st race</th>
+        <th colspan="2">Points 2nd race</th>
+		<th colspan="2">Points 3rd race</th>
+		<th>Sum</th>
+		<th>Rank</th>
+      </tr>
+	  <tr>
+		  <th></th>
+		  <th>5km</th>
+		  <th>10km</th>
+		  <th>10km</th>
+		  <th>Half Marathon</th>
+		  <th></th>
+		  <th></th>
+		  <th></th>
+		  <th></th>
+	  </tr>
+    </table>
+	<h2>18/10: 5 km</h2>
+	<table id='five' style="width:100%">
+      <tr>
+        <th>Name</th>
+		<th>Wheel Size</th>
+		<th>Crank Size</th>
+		<th>Time</th>
+		<th>Rank</th>
+      </tr>
+	</table>
+	<h2>18/10: 10 km</h2>
+	<table id='first-ten' style="width:100%">
+      <tr>
+        <th>Name</th>
+		<th>Wheel Size</th>
+		<th>Crank Size</th>
+		<th>Time</th>
+		<th>Rank</th>
+      </tr>
+	</table>
+	<h2>15/11: 10km</h2>
+	<table id='second-ten' style="width:100%">
+      <tr>
+        <th>Name</th>
+		<th>Wheel Size</th>
+		<th>Crank Size</th>
+		<th>Time</th>
+		<th>Rank</th>
+      </tr>
+	</table>
+	<h2>15/11: Half Marathon</h2>
+	<table id='half-marathon' style="width:100%">
+      <tr>
+        <th>Name</th>
+		<th>Wheel Size</th>
+		<th>Crank Size</th>
+		<th>Time</th>
+		<th>Rank</th>
+      </tr>
+    </table>
+</html>
